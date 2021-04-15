538478a56aa6ecb0c88579558efa64797ef887ed
Arunangshu Pradhan <arunangshupradhan17@gmail.com>
2021-04-15 22:29:00 +0530

    rrr

--- school/config/autoload.php
+++ school/config/autoload.php
@@ -1,6 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
 test
+change from branch 1
 | -------------------------------------------------------------------
 | AUTO-LOADER
 | -------------------------------------------------------------------
